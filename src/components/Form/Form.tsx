b99dc1b8636cf07b6d582c7895b7a2341acad1e2
anik-pas <anik-pas@ya.ru>
2026-03-24 14:33:42 +0300

    2 lesson

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,13 @@
+import './Form.scss'
+export const Form = () => {
+  return (
+    <div className="form-wrapper">
+      <form action="#">
+        <label>
+          <input type="text" />
+          <button type="button"></button>
+        </label>
+      </form>
+    </div>
+  )
+}
