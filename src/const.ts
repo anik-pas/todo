09ebc403487375da08727b6fee0f0baca22067fc
anik-pas <anik-pas@ya.ru>
2026-04-01 12:18:05 +0300

    "Исправление ghRoot - извлекаем путь из URL"

--- src/const.ts
+++ src/const.ts
@@ -2,4 +2,13 @@ import packageJson from "../package.json";
 
 const isDev = process.env.NODE_ENV === "development";
 
-export const ghRoot = isDev ? "" : packageJson.homepage;
+const getPathFromUrl = (url: string): string => {
+  try {
+    const parsed = new URL(url);
+    return parsed.pathname; 
+  } catch {
+    return url;
+  }
+};
+
+export const ghRoot = isDev ? "" : getPathFromUrl(packageJson.homepage);
